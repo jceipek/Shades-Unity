701ccc9d88a6ace93b54cf986027dd7c37eb21b2
Joseph Gibson <joseph.gibson@students.olin.edu>
2012-03-13 15:21:45 -0400

    Adding basic movement and fixing jumping- he flies

--- Shades/Assets/AvatarMotion.js
+++ Shades/Assets/AvatarMotion.js
@@ -2,11 +2,25 @@
 
 var jumping;
 
+var grounded;
+
+var maxSpeed : float;
+
 function Start () {
 	jumping = false;
+	grounded = false;
+	maxSpeed = 10;
 }
 
 function Update () {
+	if (Mathf.Abs(rigidbody.velocity.y) < maxSpeed){
+		rigidbody.velocity.y = maxSpeed * Mathf.Sign(rigidbody.velocity.y);
+	}
+
+	if (! grounded){
+		rigidbody.AddForce(0, -10, 0);
+	}
+
 	if (Input.GetButtonDown("Jump")) {
 		jumping = true;
 	}
@@ -15,7 +29,23 @@ function Update () {
 		jumping = false;
 	}
 	
-	if(jumping) {
-		transform.Translate(0, 9 * Time.deltaTime, 0);
+	if(grounded && jumping) {
+		rigidbody.AddForce(0, 1000, 0);
+		grounded = false;
+	}
+
+	if (Input.GetKey(KeyCode.LeftArrow)) {
+		transform.Translate(-9 * Time.deltaTime, 0, 0);
+	}
+
+	if (Input.GetKey(KeyCode.RightArrow)) {
+		transform.Translate(9 * Time.deltaTime, 0, 0);
+	}
+}
+
+
+function OnCollisionEnter(theCollision : Collision) {
+	if(theCollision.gameObject.name == "PlatformObj"){
+		grounded = true;
 	}
 }
