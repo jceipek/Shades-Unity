f0b83ca333461cd9ee235ed8eee6b21d1216f022
Julian Ceipek <julian.ceipek@gmail.com>
2012-04-19 00:43:26 -0400

    Made parallax more legit (still jerky, though :() and implemented Joe's suggestion about walking speed - implemented mathematically to match walk distance.

--- Shades/Assets/Scripts/ParallaxScrollController.js
+++ Shades/Assets/Scripts/ParallaxScrollController.js
@@ -1,12 +1,22 @@
 #pragma strict
 
-private var offset : float;
 var sensitivity : float = 0.8;
+var springiness = 4.0;
+var smoothTime = 100.0;
+
+private var initCamPos : float;
+private var initScrollLayerPos : float;
 
 function Awake () {
-	offset = Camera.main.transform.position.x-transform.position.x;
+	initCamPos = Camera.main.transform.position.x;
+	initScrollLayerPos = transform.position.x;
 }
 
 function Update () {
-	transform.position.x = Camera.main.transform.position.x - (Camera.main.transform.position.x-offset)*sensitivity;
+	// Where should our camera be looking right now?
+	//var goalPosition : Vector3 = Camera.main.GetComponent(CameraScrolling).GetGoalPosition();
+	//var actualCamPosX : float = Vector3.Lerp (transform.position, goalPosition, Time.deltaTime * springiness).x;
+	var actualCamPosX = Camera.main.transform.position.x;
+	
+	transform.position.x = Mathf.Lerp(transform.position.x, sensitivity * (actualCamPosX - initCamPos) + initScrollLayerPos, Time.deltaTime * smoothTime);
 }
