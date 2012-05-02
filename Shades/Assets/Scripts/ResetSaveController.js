31ea428743a68ede499716ce5f6789d48badd45d
Julian Ceipek <julian.ceipek@gmail.com>
2012-05-02 15:06:52 -0400

    reset on win

---
+++ Shades/Assets/Scripts/ResetSaveController.js
@@ -0,0 +1,5 @@
+#pragma strict
+
+function Start () {
+	PlayerPrefs.DeleteKey("Level");
+}
--- Shades/Assets/Scripts/TimedDeathController.js
+++ Shades/Assets/Scripts/TimedDeathController.js
@@ -1,16 +1,8 @@
 #pragma strict
 
 var secondsToExist : float = 3.0f;
-private var startTime : float;
 
-function Awake() {
-	startTime = Time.time;
-}
-
-function Update(){
-  // if secondsToExist have passed since the timer was started
-  if(Time.time - startTime >= secondsToExist){
-    // destroy the gameobject this script is attached to
-    Destroy(gameObject);
-  }
+function Start() {
+	yield WaitForSeconds(secondsToExist);
+	Destroy(gameObject);
 }
