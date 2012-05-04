5d419fa4ac7e63a4f791fc21b0e758156a47e797
Joseph Gibson <joseph.gibson@students.olin.edu>
2012-05-04 01:49:24 -0400

    Working enemy movement. Enemy doesn't hit deadzone.

--- Shades/Assets/Scripts/EnemyController.js
+++ Shades/Assets/Scripts/EnemyController.js
@@ -1,62 +1,81 @@
 #pragma strict
 
 var movementSpeed : float = 1.0f;
+var gravity : float = 10.0f;
 
 @System.NonSerialized
 var avatar : GameObject;
-@System.NonSerialized
-var collisionFlags : boolean;
-@System.NonSerialized
+//@System.NonSerialized
 var direction : int = 1;
-@System.NonSerialized
-var overPlatformBefore : boolean = true;
+//@System.NonSerialized
+var overPlatformBefore : boolean = false;
 @System.NonSerialized
 var movementOffset = Vector3(2, 0, 0);
 @System.NonSerialized
 var controller : CharacterController;
+@System.NonSerialized
+var collisionFlags : boolean;
+//@System.NonSerialized
+var overPlatform : boolean = true;
+@System.NonSerialized
+var rightLayer;
+
 
 function Awake() {
-	// Called before any Start functions and lets us initialize stuff. Always called (but only once) even if object is disabled, so use this for construction.	
-	//controller = GetComponent(CharacterController);
-		
-	// Always ignore physics of world avatar is not in. XXX: Not sure if this is the right place to put this code...
-	//Physics.IgnoreLayerCollision(LayerMask.NameToLayer("DarkWorld"),LayerMask.NameToLayer("LightWorld"),true);
-	//Physics.IgnoreLayerCollision(LayerMask.NameToLayer("LightWorld"),LayerMask.NameToLayer("DarkWorld"),true);
-	
-	//CorrectTexture();
-	
 	
 	controller = GetComponent(CharacterController);
 	
 	avatar = GameObject.Find("Avatar");
 	
-
+	if (gameObject.layer == LayerMask.NameToLayer("LightWorld")) {
+		rightLayer = LayerMask.NameToLayer("LightWorld");
+	} else if (gameObject.layer == LayerMask.NameToLayer("DarkWorld")) {
+		rightLayer = LayerMask.NameToLayer("DarkWorld");
+	}
 }
 
 function Update() {
-	var down = Vector3(0, -2, 0);
+	var down = Vector3(0, -.5, 0);
+	var hit : RaycastHit;
 	
-	if (!Physics.Raycast(transform.position, down, 2) && overPlatformBefore) {
-		direction *= -1;
-		overPlatformBefore = false;
+	overPlatform = Physics.Raycast(transform.position, down, hit, 2);
+	
+	Debug.Log(collider.gameObject.layer);
+	
+	if (!overPlatform) {
+		if (overPlatformBefore) {
+			direction *= -1;
+			overPlatformBefore = false;
+		}
+		movementOffset += direction * gravity * down;
+		
     } else {
-		if (Physics.Raycast(transform.position, down, 2) && !overPlatformBefore) {
-			overPlatformBefore = true;
+		if (overPlatform) {
+			if (hit.collider.gameObject.layer == rightLayer) {
+				overPlatformBefore = true;
+			} else {
+				movementOffset += direction * gravity * down;
+				if (overPlatformBefore) {
+					direction = -direction;
+				}
+			}
 		}
 	}
 	
 	EnemyMove();
 	
 	var distance = Vector3.Distance(avatar.transform.position, transform.position);
-	if (distance < 3) {
-		avatar.gameObject.SendMessage("OnDeath", SendMessageOptions.DontRequireReceiver);
+	if (avatar.gameObject.layer == gameObject.layer) {
+		if (distance < 3) {
+			avatar.gameObject.SendMessage("OnDeath", SendMessageOptions.DontRequireReceiver);
+		}
 	}
 }
 
-function OnTriggerEnter(other : Collider) {
-	other.gameObject.SendMessage("OnDeath", SendMessageOptions.DontRequireReceiver);
-}
 
 function EnemyMove() {
-	collisionFlags = controller.Move(movementOffset * movementSpeed * direction * Time.deltaTime);
+	movementOffset *= movementSpeed * direction * Time.deltaTime;
+	collisionFlags = controller.Move(movementOffset);
+	movementOffset = Vector3(2, 0, 0);
 }
+
