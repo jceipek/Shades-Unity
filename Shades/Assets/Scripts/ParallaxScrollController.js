89816a13a48bcf58d6b8f0b425b3f00044bba192
Julian Ceipek <julian.ceipek@gmail.com>
2012-05-07 21:20:09 -0400

    Modified parallax script to change with y as well as x. Modified skybox.

--- Shades/Assets/Scripts/ParallaxScrollController.js
+++ Shades/Assets/Scripts/ParallaxScrollController.js
@@ -4,12 +4,14 @@ var sensitivity : float = 0.8;
 var springiness = 4.0;
 var smoothTime = 100.0;
 
-private var initCamPos : float;
-private var initScrollLayerPos : float;
+var changeY : boolean = false;
+
+private var initCamPos : Vector3;
+private var initScrollLayerPos : Vector3;
 
 function Awake () {
-	initCamPos = Camera.main.transform.position.x;
-	initScrollLayerPos = transform.position.x;
+	initCamPos = Camera.main.transform.position;
+	initScrollLayerPos = transform.position;
 }
 
 function LateUpdate() {
@@ -18,7 +20,10 @@ function LateUpdate() {
 
 
 function SetToGoal() {
-	var actualCamPosX : float = Camera.main.transform.position.x;
-	transform.position.x = sensitivity * (actualCamPosX - initCamPos) + initScrollLayerPos;
+	var actualCamPos : Vector3 = Camera.main.transform.position;
+	transform.position.x = sensitivity * (actualCamPos.x - initCamPos.x) + initScrollLayerPos.x;
 
+	if (changeY) {
+		transform.position.y = sensitivity * (actualCamPos.y - initCamPos.y) + initScrollLayerPos.y;
+	}
 }
